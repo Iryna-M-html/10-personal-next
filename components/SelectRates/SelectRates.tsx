0c750a327e65fa5156f8541b033fc47fd73706a6
Iryna-M-html <ivmohnataya@gmail.com>
2025-10-24 09:26:29 +0300

    selectrates comented

--- components/SelectRates/SelectRates.tsx
+++ components/SelectRates/SelectRates.tsx
@@ -1,33 +1,33 @@
-'use client';
+// 'use client';
 
-import { useCurrencyStore } from '@/lib/stores/currencyStore';
+// import { useCurrencyStore } from '@/lib/stores/currencyStore';
 
-const availableCurrencies = ['USD', 'EUR', 'UAH', 'GBP', 'JPY'];
+// const availableCurrencies = ['USD', 'EUR', 'UAH', 'GBP', 'JPY'];
 
-export default function CurrencySelector() {
-  const { currency, setCurrency } = useCurrencyStore();
+// export default function CurrencySelector() {
+//   const { currency, setCurrency } = useCurrencyStore();
 
-  return (
-    <div style={{ marginTop: '1rem' }}>
-      <label htmlFor="currency">Choose currency: </label>
-      <select
-        id="currency"
-        value={currency || ''}
-        onChange={(e) => {
-          setCurrency(e.target.value);
-          localStorage.setItem('user_currency', e.target.value); // ✅ сохраняем вручную выбранную валюту
-        }}
-      >
-        <option value="">Select currency</option>
-        {availableCurrencies.map((cur) => (
-          <option key={cur} value={cur}>
-            {cur}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
-}
+//   return (
+//     <div style={{ marginTop: '1rem' }}>
+//       <label htmlFor="currency">Choose currency: </label>
+//       <select
+//         id="currency"
+//         value={currency || ''}
+//         onChange={(e) => {
+//           setCurrency(e.target.value);
+//           localStorage.setItem('user_currency', e.target.value); // ✅ сохраняем вручную выбранную валюту
+//         }}
+//       >
+//         <option value="">Select currency</option>
+//         {availableCurrencies.map((cur) => (
+//           <option key={cur} value={cur}>
+//             {cur}
+//           </option>
+//         ))}
+//       </select>
+//     </div>
+//   );
+// }
 
 // import Select, { SingleValue } from 'react-select';
 
